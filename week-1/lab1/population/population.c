1596302800634c242e959e90ab5c72b81eeb136f
ohnearbeit <ericcb2018@gmail.com>
2023-09-21 13:21:16 -0300

    Add lab 1 problem

---
+++ week-1/lab1/population/population.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    // Reprompt if < 9
+
+    // TODO: Prompt for end size
+    // Reprompt if < start size
+
+    // TODO: Calculate number of years until we reach threshold
+    // Each year, n / 3 new llamas are born, and n / 4 llamas pass away.
+
+    // TODO: Print number of years
+}
