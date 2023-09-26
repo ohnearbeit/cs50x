eb1852cbb8d833b0ff7639825889f28fa072f7d7
ohnearbeit <ericcb2018@gmail.com>
2023-09-26 14:06:02 -0300

    Implement Problem set 1, Mario

--- week-1/problem-set-1/mario/mario.c
+++ week-1/problem-set-1/mario/mario.c
@@ -3,5 +3,27 @@
 
 int main(void) 
 {
+    // Prompt the user for a height from 1 to 8
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8);
 
+    for (int i = 0; i < height; i++)
+    {
+        // Pad left side with spaces
+        for (int n = 1; n < height - i; n++)
+        {
+                printf(" ");
+        }
+
+        // Print blocks
+        for (int j = 0; j < i + 1; j++)
+        {
+                printf("#");
+        }
+        
+        printf("\n");
+    }
 }
